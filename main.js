cb2c23ab8bb457274d82d0e2ce847e90947a788d
Alex Crichton <alex@alexcrichton.com>
2021-11-16 07:53:52 -0800

    Try to fill out

--- main.js
+++ main.js
@@ -1,8 +1,17 @@
 const core = require('@actions/core');
-const github = require('@actions/github');
+const child_process = require('child_process');
 
 try {
   const tag = core.getInput('wai-abi-tag');
+  child_process.execFileSync('cargo', [
+    'install',
+    '--git',
+    'https://github.com/alexcrichton/example-wasi-tools',
+    'wai-abi',
+    '--tag',
+    tag,
+  ]);
+  child_process.execFileSync('wai-gen', ['--check', '.'])
 } catch (error) {
   core.setFailed(error.message);
 }
