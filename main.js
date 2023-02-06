d1fe6d990175ce3027843cd8e23fe38ac4743b9a
Dan Gohman <dev@sunfishcode.online>
2023-02-06 15:58:53 -0800

    Update for the new wit-abi tool and proposed new proposal layout.
    The v7 wit-abi tool command-line interface now has a slightly different
    command-line interface. To support worlds, this envisions interfaces
    being placed in their own directory, along with a world.wit.md file
    containing a world defining them.

--- main.js
+++ main.js
@@ -27,20 +27,20 @@ try {
     core.startGroup('Use `wit-abi` to verify abi files are up to date');
     child_process.execFileSync(
       'wit-abi',
-      ['--check', '.'],
+      ['markdown', '--check', '--world=world.world' 'wit'],
       { stdio: [null, process.stdout, process.stdout] },
     );
     core.endGroup();
   } catch (error) {
     core.endGroup();
-    core.info('Failed to verify that `*.abi.md` files are up-to-date with');
-    core.info('their `*.wit.md` counterparts. The `wit-abi` tool needs to be');
+    core.info('Failed to verify that `*.md` files are up-to-date with');
+    core.info('their `wit/*.wit.md` counterparts. The `wit-abi` tool needs to be');
     core.info('rerun on this branch and the changes should be committed');
     core.info('');
     core.info(`  cargo install --git ${url} --locked wit-abi --tag ${tag}`);
-    core.info(`  wit-abi .`)
+    core.info(`  wit-abi markdown --world=world.world wit`)
     core.info('');
-    core.info('That command will regenerate the `*.abi.md` files to get committed here');
+    core.info('That command will regenerate the `*.md` files to get committed here');
     throw error;
   }
 } catch (error) {
